25b1a735f9eb6676d33f8f3069d7a157c79aa70c
betula <mail@betula.co>
2020-06-29 19:08:26 +0700

    fix: signal run twice for same value

---
+++ test/signal.test.js
@@ -0,0 +1,19 @@
+import { signal, signal_listen, extract_id } from "../lib/signal";
+
+test("should fire only once for non changed value", () => {
+  const s = signal(1);
+  const spy = jest.fn();
+  signal_listen(extract_id(s), spy);
+
+  s(1);
+  expect(spy).toHaveBeenCalledTimes(1);
+  expect(spy).toHaveBeenLastCalledWith(1);
+
+  s(1, "b");
+  s(1, "b");
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenLastCalledWith(1, "b");
+
+  s("b", "b");
+  expect(spy).toHaveBeenLastCalledWith("b", "b");
+});
