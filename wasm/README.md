20e329afa818a724af7925b3d037f261e8a1f1d2
betula <mail@betula.co>
2020-07-07 20:17:46 +0700

    chore: build wasm with wabt npm package

--- wasm/README.md
+++
@@ -1,1 +0,0 @@
-See [Converting WebAssembly text format to wasm](https://developer.mozilla.org/en-US/docs/WebAssembly/Text_format_to_wasm) for details about `wat2wasm` installation.
--- wasm/dev/index.js
+++ wasm/dev/index.js
@@ -1,30 +1,44 @@
 const fs = require("fs");
-const wasmSource = new Uint8Array(fs.readFileSync("index.wasm"));
-const wasmModule = new WebAssembly.Module(wasmSource);
-const wasmInstance = new WebAssembly.Instance(wasmModule, {
+const wabt_factory = require("wabt");
+const path = require("path");
+
+run();
+
+async function run() {
+  const wabt = await wabt_factory();
+  const input_wat = path.resolve("../lib/index.wat");
+  const wasm_module = wabt.parseWat(input_wat, fs.readFileSync(input_wat, "utf8"));
+  const { buffer } = wasm_module.toBinary({});
+  await main(Buffer.from(buffer));
+}
+
+async function main(wasm_buffer) {
+  const wasm_module = await WebAssembly.compile(wasm_buffer);
+  const wasm_instance = new WebAssembly.Instance(wasm_module, {
     env: {
         memory: new WebAssembly.Memory({
             initial: 1
         })
     }
-});
+  });
 
-const {
-  set_create,
-  set_add,
-  set_has,
-  set_delete,
-  set_extract
-} = wasmInstance.exports;
+  const {
+    set_create,
+    set_add,
+    set_has,
+    set_delete,
+    set_extract
+  } = wasm_instance.exports;
 
-const id = set_create();
-console.log("SET CREATE", id);
+  const id = set_create();
+  console.log("SET CREATE", id);
 
-set_add(id, 10);
-console.log("SET ADD", id, 10);
+  set_add(id, 10);
+  console.log("SET ADD", id, 10);
 
-if (set_has(id, 1)) {
-  set_delete(id, 1);
+  if (set_has(id, 1)) {
+    set_delete(id, 1);
+  }
+
+  console.log("EXTRACT:", set_extract());
 }
-
-console.log("EXTRACT:", set_extract());
