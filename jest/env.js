54b265dc3b279c505eaa3b6d5b0dc2e926b4b5df
betula <mail@betula.co>
2020-07-10 10:03:15 +0700

    fix: wasm init in jest tests setup

--- jest/env.js
+++ jest/env.js
@@ -1,11 +1,6 @@
 import { init } from "../lib";
 import { test_scope_start, test_scope_finish } from "../lib/test";
 
-
-beforeEach(function (done) {
-  init(function() {
-    test_scope_start();
-    done();
-  });
-});
+beforeAll(done => init(done));
+beforeEach(test_scope_start);
 afterEach(test_scope_finish);
