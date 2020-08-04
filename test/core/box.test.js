eb621665f25c29036eae52cac3814f7a3dc1e14c
betula <mail@betula.co>
2020-08-04 18:20:34 +0700

    feat: add box_deep_invalidate func test case

--- test/core/box.test.js
+++ test/core/box.test.js
@@ -1,5 +1,6 @@
 import {
-  tick_start, tick_deep_inc
+  tick_start, tick_deep_inc, box_rels, box_invalid, box_deep_invalidate,
+  set_add, set_create, set_delete, set_has, set_assign, set_clear, set_extract, set_free, map_set
 } from "../../lib/core/test";
 
 test("should throw tick_start limit error", () => {
@@ -12,3 +13,52 @@ test("should throw tick_start limit error", () => {
   }).toThrow("Tick deep limit exception")
 
 });
+
+test("should work box_deep_invalidate func", () => {
+
+  let changed = set_create();
+
+  set_add(changed, 1)
+  set_add(changed, 5)
+
+  let rels_1 = set_create()
+  let rels_2 = set_create()
+  let rels_3 = set_create()
+
+  map_set(box_rels(), 1, rels_1)
+  map_set(box_rels(), 5, rels_2)
+  map_set(box_rels(), 3, rels_3)
+
+  set_add(rels_1, 10)
+  set_add(rels_1, 11)
+  set_add(rels_2, 20)
+  set_add(rels_2, 21)
+  set_add(rels_3, 30)
+  set_add(rels_3, 31)
+
+  let rels_4 = set_create()
+  let rels_5 = set_create()
+  let rels_6 = set_create()
+
+  map_set(box_rels(), 10, rels_4)
+  map_set(box_rels(), 21, rels_5)
+  map_set(box_rels(), 31, rels_6)
+
+  set_add(rels_4, 50)
+  set_add(rels_5, 60)
+  set_add(rels_6, 70)
+
+  let rels_7 = set_create()
+  set_add(rels_7, 100)
+  set_add(rels_7, 101)
+  set_add(rels_7, 103)
+
+  map_set(box_rels(), 50, rels_7)
+
+  box_deep_invalidate(changed);
+
+  expect(set_extract(box_invalid())).toStrictEqual([
+    10, 11, 20, 21, 50, 60, 100, 101, 103
+  ]);
+});
+
--- test/core/set.test.js
+++ test/core/set.test.js
@@ -1,5 +1,5 @@
 import {
-  set_add, set_create, set_delete, set_has, set_extract, set_free
+  set_add, set_create, set_delete, set_has, set_assign, set_clear, set_extract, set_free
 } from "../../lib/core/test";
 
 let expected_id = 0;
@@ -71,6 +71,24 @@ test("should work set grow", () => {
   }
 
   expect(set_extract(k)).toStrictEqual([
+    0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+  ]);
+});
+
+test("should work set assign", () => {
+  let a = set_create();
+  for (let i = 0; i < 40; i++) {
+    set_add(a, i);
+  }
+  let b = set_create();
+  set_assign(b, a)
+  set_clear(a)
+
+  expect(set_extract(a)).toStrictEqual([]);
+  expect(set_extract(b)).toStrictEqual([
     0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
     10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
     20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
