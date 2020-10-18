8ed279faa5bce50652c68779e73743575345fed5
betula <mail@betula.co>
2020-10-19 03:57:56 +0700

    chore: remove assemply, jest snapshot resolver, prep

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
-import assembly from "./assembly/rollup-plugin";
-import prep from "./prep/rollup-plugin";
 import pkg, { peerDependencies } from "./package.json";
 
 export default {
@@ -16,8 +14,6 @@ export default {
   external: Object.keys(peerDependencies),
   plugins: [
     resolve(),
-    assembly(),
-    prep(),
     ...(
       process.env.REALAR_BUILD_TERSER
         ? [terser()]
