c87d21517d03cde75037181df00525376490cc94
betula <mail@betula.co>
2020-10-19 14:27:21 +0700

    feat: typing tests fixed

--- types/typescript/check.tsx
+++ types/typescript/check.tsx
@@ -3,21 +3,12 @@ import {
   unit,
   useOwn,
   useShared,
-  action,
-  signal,
-  call,
   shared,
   Shared,
-  Scope,
   changed,
   pending,
-  on,
   effect,
-  ready,
 
-  Argument,
-  Result,
-  Handler,
   AsyncPool
 } from "../../build";
 
@@ -26,59 +17,12 @@ interface Item {
   completed?: boolean
 }
 
-const empty = action();
-const doIt = action<number>();
-const doIt_2 = doIt;
-const shareIt = signal<string>();
-const shareIt_2 = shareIt;
-const callIt = call<number[][], number[]>();
-const callIt_2 = callIt;
-
-empty();
-doIt(10);
-shareIt("hello");
-f(callIt([[4, 5]]));
-
-function f(r: number[]) {
-  return r;
-}
-
 const Router = unit({
   path: null,
 });
 
 const Unit = unit({
   service: shared(Router),
-
-  [doIt]: (id: Argument<typeof doIt>) => {
-    function f(n: number) {
-      return n;
-    }
-    return f(id);
-  },
-  [doIt_2]: function() {} as Handler<typeof doIt_2>,
-
-  [shareIt]: function(text: Argument<typeof shareIt>) {
-    function f(s: string) {
-      return s;
-    }
-    return f(text);
-  },
-  [shareIt_2]: function() {} as Handler<typeof shareIt_2>,
-
-  [callIt](req: Argument<typeof callIt>): Result<typeof callIt> {
-    function f(m: number[][]) {
-      return m[0];
-    }
-    return f(req);
-  },
-  [callIt_2]: function(arg) {
-    function f(m: number[][]) {
-      return m[0];
-    }
-    return f(arg);
-  } as Handler<typeof callIt_2>,
-
 
   todos: [] as Item[],
   disabled: null as boolean | null,
@@ -102,9 +46,6 @@ const Unit = unit({
   constructor(initial?: Item[]) {
     this.todos = initial ?? this.todos;
 
-    on(doIt, (n) => n.toFixed());
-    on(shareIt, (s) => s.charCodeAt(0));
-    on(callIt, (matrix) => matrix[0][0].toFixed());
     effect(() => {});
     effect(() => () => {});
   },
@@ -117,13 +58,6 @@ const Unit = unit({
     (this.load as AsyncPool).pending || pending(this.load);
   },
 });
-
-
-ready((n, s) => {
-  return n.toFixed() + s.charAt(0);
-}, 10, "hello");
-
-ready(() => {});
 
 export const App = () => {
 
@@ -142,9 +76,9 @@ export const App = () => {
   useShared(Unit).load.pending;
 
   return (
-    <Scope>
+    <>
       <Shared unit={Unit} />
       <Shared unit={Router} />
-    </Scope>
+    </>
   );
 };
