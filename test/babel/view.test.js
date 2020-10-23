ad4c46d1fb3f3fbada90d2953b4ec784154837a4
betula <mail@betula.co>
2020-10-23 16:45:58 +0700

    chore: failed test added

--- test/babel/view.test.js
+++ test/babel/view.test.js
@@ -3,12 +3,6 @@ import { plugin } from "../../babel/plugin";
 import { view_unit_name } from "../../babel/view-transform";
 
 const
-  box_expr_create = 0,
-  box_expr_start = 1,
-  box_expr_finish = 2,
-  box_computed_create = 3,
-  box_computed_start = 4,
-  box_computed_finish = 5,
   box_entry_start = 6,
   box_entry_finish = 7;
 
@@ -37,7 +31,7 @@ test("should work unit inside func with JSX", () => {
   let _c_unit_v = ${view_unit_name},
       _c_ret_tmp;
 
-  _c_unit_v[${box_expr_create}]();
+  _c_unit_v[0]();
 
   const u_f = unit(function () {
     let _core = unit.c;
@@ -55,90 +49,6 @@ test("should work unit inside func with JSX", () => {
 
   _c_unit_v[1]();
 });`;
-  expect(transform(code)).toBe(expected);
-});
-
-test("should transform nested functions JSX", () => {
-  const code = `
-    function _() {
-      function A() {
-        return <div />;
-      }
-    }
-  `;
-  const expected = `function _() {
-  function A() {
-    let _c_unit_v = ${view_unit_name},
-        _c_ret_tmp;
-
-    _c_unit_v[0]();
-
-    return _c_ret_tmp = <div />, _c_unit_v[1](), _c_ret_tmp;
-  }
-}`;
-  expect(transform(code)).toBe(expected);
-});
-
-test("should transform JSX functions with JSX functions", () => {
-  const code = `
-    function _() {
-      function A() {
-        function B() {
-          return <div />;
-        }
-        return <div />;
-      }
-    }
-  `;
-  const expected = `function _() {
-  function A() {
-    let _c_unit_v2 = ${view_unit_name},
-        _c_ret_tmp2;
-
-    _c_unit_v2[0]();
-
-    function B() {
-      let _c_unit_v = ${view_unit_name},
-          _c_ret_tmp;
-
-      _c_unit_v[0]();
-
-      return _c_ret_tmp = <div />, _c_unit_v[1](), _c_ret_tmp;
-    }
-
-    return _c_ret_tmp2 = <div />, _c_unit_v2[1](), _c_ret_tmp2;
-  }
-}`;
-  expect(transform(code)).toBe(expected);
-});
-
-test("should transform expression functions JSX", () => {
-  const code = `
-    export const App = function() {
-      useShared(Unit);
-      return null;
-    }
-    export const H1 = function() {
-      return <h1 />;
-    }
-  `;
-  const expected = `export const App = function () {
-  let _c_unit_v = ${view_unit_name},
-      _c_ret_tmp;
-
-  _c_unit_v[0]();
-
-  useShared(Unit);
-  return _c_ret_tmp = null, _c_unit_v[1](), _c_ret_tmp;
-};
-export const H1 = function () {
-  let _c_unit_v2 = ${view_unit_name},
-      _c_ret_tmp2;
-
-  _c_unit_v2[0]();
-
-  return _c_ret_tmp2 = <h1 />, _c_unit_v2[1](), _c_ret_tmp2;
-};`;
   expect(transform(code)).toBe(expected);
 });
 
@@ -185,50 +95,65 @@ export const A = ({
   expect(transform(code)).toBe(expected);
 });
 
-test("should transform JSX manipulations", () => {
-  const code = `function Whirl({ children }) {
-    const { map, shift, push } = useOwn(whirl);
-    return (
-      <>
-        <button onClick={shift}>-</button>
-        {map(key => (
-          <div key={key}>
-            {children}
-          </div>
-        ))}
-        <button onClick={push}>+</button>
-      </>
-    )
-  }`;
-  const expected = `function Whirl({
-  children
+test("should transform expression functions JSX", () => {
+  const code = `
+    export const App = function() {
+      useShared(Unit);
+      return null;
+    }
+    export const H1 = function() {
+      return <h1 />;
+    }
+  `;
+  const expected = `export const App = function () {
+  let _c_unit_v = ${view_unit_name},
+      _c_ret_tmp;
+
+  _c_unit_v[0]();
+
+  useShared(Unit);
+  return _c_ret_tmp = null, _c_unit_v[1](), _c_ret_tmp;
+};
+export const H1 = function () {
+  let _c_unit_v2 = ${view_unit_name},
+      _c_ret_tmp2;
+
+  _c_unit_v2[0]();
+
+  return _c_ret_tmp2 = <h1 />, _c_unit_v2[1](), _c_ret_tmp2;
+};`;
+  expect(transform(code)).toBe(expected);
+});
+
+
+test("should transform nested functions JSX as render functions", () => {
+  const code = `
+    function _({ p }) {
+      function a(v) {
+        function br() {
+          return <br />;
+        }
+        return <a>{v}{br()}</a>;
+      }
+      return p.map(v => <div>{a(v)}</div>);
+    }
+  `;
+  const expected = `function _({
+  p
 }) {
   let _c_unit_v = ${view_unit_name},
       _c_ret_tmp;
 
   _c_unit_v[0]();
 
-  const {
-    map,
-    shift,
-    push
-  } = useOwn(whirl);
-  return _c_ret_tmp = <>
-        <button onClick={shift}>-</button>
-        {map(key => {
-      let _c_unit_v2 = ${view_unit_name},
-          _c_ret_tmp2;
-
-      _c_unit_v2[0]();
-
-      return _c_ret_tmp2 = <div key={key}>
-            {children}
-          </div>, _c_unit_v2[1](), _c_ret_tmp2;
-    })}
-        <button onClick={push}>+</button>
-      </>, _c_unit_v[1](), _c_ret_tmp;
+  function a(v) {
+    function br() {
+      return <br />;
+    }
+    return <a>{v}{br()}</a>;
+  }
+  return _c_ret_tmp = p.map(v => <div>{a(v)}</div>), _c_unit_v[1](), _c_ret_tmp;
 }`;
   expect(transform(code)).toBe(expected);
 });
-
 
