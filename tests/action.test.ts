844fbf1b81a7689663821cc3d69b04bf45f8d508
betula <mail@betula.co>
2021-01-22 17:21:27 +0700

    chore: action as promise test added

--- tests/action.test.ts
+++ tests/action.test.ts
@@ -1,6 +1,6 @@
 import { action, cycle } from '../src';
 
-test('should work action', () => {
+test('should work action in cycle', () => {
   const spy = jest.fn();
 
   const a = action<number>();
@@ -16,4 +16,25 @@ test('should work action', () => {
   a(10);
   expect(spy).toHaveBeenNthCalledWith(3, 10, true);
   expect(spy).toBeCalledTimes(3);
+});
+
+test('should work action as promise', async () => {
+  const spy = jest.fn();
+
+  const a = action<number>();
+  const fn = async () => {
+    spy(await a);
+  };
+  fn();
+  expect(spy).toBeCalledTimes(0);
+  a(10);
+  await new Promise(resolve => setTimeout(resolve, 10));
+  expect(spy).toHaveBeenNthCalledWith(1, 10);
+  expect(spy).toBeCalledTimes(1);
+  fn();
+  expect(spy).toBeCalledTimes(1);
+  a(10);
+  await new Promise(resolve => setTimeout(resolve, 10));
+  expect(spy).toBeCalledTimes(2);
+  expect(spy).toHaveBeenNthCalledWith(2, 10);
 });
