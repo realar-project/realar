43b4206f468d3a5e1295646a67e4b98f00e67d6c
betula <mail@betula.co>
2021-01-28 14:12:53 +0700

    chore: increase coverage

---
+++ tests/prop.test.ts
@@ -0,0 +1,14 @@
+import { prop, on } from '../src';
+
+test('should work prop decorator with first set', () => {
+  const spy = jest.fn();
+  class A {
+    @prop b = 0;
+  }
+  const a = new A();
+  a.b = 10;
+  on(() => a.b, spy);
+  a.b = 11;
+  expect(spy).toHaveBeenNthCalledWith(1, 11, 10);
+  expect(spy).toBeCalledTimes(1);
+});
