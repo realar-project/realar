e766e27a2a9c849a763fd89c57b075e82b41624f
betula <mail@betula.co>
2021-01-29 23:14:30 +0700

    feat: add sync test

---
+++ tests/sync.test.ts
@@ -0,0 +1,33 @@
+import { prop, cache, sync, box } from '../src';
+
+test('should work basic operations with prop, cache and sync', () => {
+  const spy = jest.fn();
+  class A {
+    @prop a = 10;
+    @cache get b() {
+      return this.a + 1;
+    }
+  }
+  const a = new A();
+  sync(() => a.b, spy);
+  expect(spy).toBeCalledTimes(1);
+  expect(spy).toHaveBeenNthCalledWith(1, 11);
+
+  a.a += 10;
+  expect(spy).toHaveBeenNthCalledWith(2, 21, 11);
+  expect(spy).toBeCalledTimes(2);
+});
+
+test('should cache return value in sync', () => {
+  const spy = jest.fn();
+  const a = box(0);
+
+  sync(() => Math.floor(a[0]() / 2), spy);
+  expect(spy).toBeCalledTimes(1);
+  expect(spy).toHaveBeenNthCalledWith(1, 0);
+
+  a[1](1);
+  a[1](2);
+  expect(spy).toHaveBeenNthCalledWith(2, 1, 0);
+  expect(spy).toBeCalledTimes(2);
+});
